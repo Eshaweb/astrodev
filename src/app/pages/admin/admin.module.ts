deb57128690a2d7ab285077b28eb081ece5aa94a
ShaileshBhatNP <shailesh81094@gmail.com>
2019-02-22 12:56:24 +0530

    With Bug Fix in Price List

--- src/app/pages/admin/admin.module.ts
+++ src/app/pages/admin/admin.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { NgxLoadingModule } from 'ngx-loading';
-import { DxButtonModule, DxCheckBoxModule, DxTextBoxModule, DxSelectBoxModule, DxLoadPanelModule, DxResponsiveBoxModule, DxPopupModule, DxPopoverModule, DxAccordionModule, DxTemplateModule, DxSliderModule, DxTagBoxModule, DxDataGridModule } from 'devextreme-angular';
+import { DxButtonModule, DxCheckBoxModule, DxTextBoxModule, DxSelectBoxModule, DxLoadPanelModule, DxResponsiveBoxModule, DxPopupModule, DxPopoverModule, DxAccordionModule, DxTemplateModule, DxSliderModule, DxTagBoxModule, DxDataGridModule, DxNumberBoxModule } from 'devextreme-angular';
 import { DxiRowModule } from 'devextreme-angular/ui/nested/row-dxi';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin/admin.component';
@@ -19,6 +19,7 @@ import { PriceListComponent } from './pricelist/pricelist.component';
         DxCheckBoxModule,
         DxButtonModule,
         DxTextBoxModule,
+        DxNumberBoxModule,
         DxSelectBoxModule,DxAccordionModule,
         DxCheckBoxModule,
         DxSliderModule,
