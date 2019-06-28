f5e29b40a84988152ad3e26d40b0ef7ede9bd757
ShaileshBhatNP <shailesh81094@gmail.com>
2019-06-28 10:34:25 +0530

    Baby naming added

---
+++ src/Models/BabyNaming/BabyNamingRequest.ts
@@ -0,0 +1,18 @@
+export class BabyNamingRequest
+{
+    Date:string;
+    Time:string;
+    Gender:string;
+    TimeFormat:string;
+    LatDeg:number;
+    LatMt:number;
+    LongDeg:number;
+    LongMt:number;
+    NS:string;
+    EW:string;
+    ZH:number;
+    ZM:number;
+    PN:string;
+    Place:string;
+    PartyMastId:string;
+}
--- src/app/pages/staticforms/contactus/contactus.component.ts
+++ src/app/pages/staticforms/contactus/contactus.component.ts
@@ -81,7 +81,7 @@ export class ContactusComponent implements OnInit {
     // else{
     //   this.siteKey='vvvvv';
     // }
-    this.siteKey="xxxxx";
+    this.siteKey="6LdY0agUAAAAAP-HXxxSih7LGcmniT2ainnJCFK0";
       //this.siteKey=LoginService.siteKey;
     
   }
